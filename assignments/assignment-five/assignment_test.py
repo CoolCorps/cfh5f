6feb91db2ff455d101b6ed9ec5364fad56b8513d
callum <calxferg@gmail.com>
2020-02-23 14:26:43 -0600

    first 8 tests

--- assignments/assignment-five/assignment_test.py
+++ assignments/assignment-five/assignment_test.py
@@ -41,23 +41,56 @@ def test_change_grade(grading_system):
 
 #4
 def test_create_assignment(grading_system):
-    pass
+    grading_system.login('cmhbf5', 'bestTA')
+
+    assignment = 'assignment3'
+    dueDate = '04/01/20'
+    course = 'cloud_computing'
+
+    grading_system.usr.create_assignment(assignment, dueDate, course)
+    
+    assert assignment in grading_system.courses[course]
+    assert grading_system.courses[course][assignment][dueDate] == dueDate
 
 #5
 def test_add_student(grading_system):
-    pass
+    grading_system.login('goggins', 'augurrox')
+
+    grading_system.usr.add_student('yted91', 'databases')
+
+    assert 'databases' in grading_system.users['yted91']['courses']
 
 #6
 def test_drop_student(grading_system):
-    pass
+    grading_system.login('goggins', 'augurrox')
+
+    grading_system.usr.drop_student('hdjsr7', 'databases')
+
+    assert 'databases' not in grading_system.users['hdjsr7']['courses']
 
 #7
 def test_submit_assignment(grading_system):
-    pass
+    user = 'hdjsr7'
+    assignmentName = 'assignment1'
+    course = 'cloud_computing'
+    submission = 'Blahhhhh'
+    date = '03/01/20'
+
+    grading_system.login(user, 'pass1234')
+
+    grading_system.usr.submit_assignment(course, assignmentName,submission, date)
+
+    assignment = grading_system.users[user]['courses'][course][assignmentName]
+
+    assert assignment['submission_date'] == date
+    assert assignment['submission'] == submission
 
 #8
 def test_check_ontime(grading_system):
-    pass
+    grading_system.login('hdjsr7', 'pass1234')
+
+    assert grading_system.usr.check_ontime('02/23/20', '02/25/20')
+    assert not grading_system.usr.check_ontime('02/23/20', '02/21/20')
 
 #9
 def test_check_grades(grading_system):
