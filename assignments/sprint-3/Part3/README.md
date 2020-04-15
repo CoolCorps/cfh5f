6967102a3ed777bb2d4a13e5e5787d5179b99478
Callum <Calxferg@gmail.com>
2020-04-15 00:13:03 -0400

    sprint-3 finished

---
+++ assignments/sprint-3/Part3/README.md
@@ -0,0 +1,11 @@
+# Sprint-3 Part 3, Open Source Project
+
+## Description
+The purpose of the project is to analyze cases of Covid-19 in the United States. Cases are viewed by state in a line graph either logarithmically or normally.
+
+## Data
+Data is pulled from here whenever the page is refreshed:
+https://github.com/nytimes/covid-19-data
+
+## How to Run
+This project is a static html/js webpage that pulls data directly from github. It can be run with no setup locally by downloading this repository then double clicking index.html, or you can view the website which is hosted here: http://4320website.com.s3-website.us-east-2.amazonaws.com/
--- assignments/sprint-3/Part3/index.js
+++ assignments/sprint-3/Part3/index.js
@@ -1,14 +1,18 @@
 $(() => {
     let covidData = null
 
-    //const url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
-    const url = "data.csv"
+    const url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
+    //const url = "data.csv"
     $.get(url, (data) => {
         covidData = parseCSV(data)
         loadStateData(covidData, $("#stateSelect").val())
     })
 
     $("#stateSelect").on("change", () => {
+        loadStateData(covidData, $("#stateSelect").val())
+    })
+
+    $("#logcheckbox").on("change", () => {
         loadStateData(covidData, $("#stateSelect").val())
     })
 })
@@ -19,8 +23,10 @@ function parseCSV(csv) {
     lines.shift()
     lines.forEach(line => {
         let lineSplit = line.split(",")
+        let date = lineSplit[0].split("-")
+        let dateObject = new Date(parseInt(date[0]), parseInt(date[1]) - 1, parseInt(date[2]))
         covidData.push({
-            date: lineSplit[0],
+            date: dateObject,
             state: lineSplit[1],
             fips: lineSplit[2],
             cases: lineSplit[3],
@@ -36,33 +42,30 @@ function loadStateData(covidData, state) {
         if(entry.state == state)
             stateData.push(entry)
     })
+
+    let dataPoints = []
+    stateData.forEach(entry => {
+        dataPoints.push({
+            x: entry.date,
+            y: parseInt(entry.cases)
+        })
+    })
     
-    var chart = new CanvasJS.Chart("chartContainer", {
+    var chart = new CanvasJS.Chart("chart", {
         animationEnabled: true,
         theme: "light2",
         title:{
-            text: "Simple Line Chart"
+            text: "Covid-19 cases in " + state
+        },
+        axisX:{
+            valueFormatString: "MMM D"
         },
         axisY:{
-            includeZero: false
+            logarithmic: $("#logcheckbox").is(":checked")
         },
         data: [{        
             type: "line",
-              indexLabelFontSize: 16,
-            dataPoints: [
-                { y: 450 },
-                { y: 414},
-                { y: 520, indexLabel: "\u2191 highest",markerColor: "red", markerType: "triangle" },
-                { y: 460 },
-                { y: 450 },
-                { y: 500 },
-                { y: 480 },
-                { y: 480 },
-                { y: 410 , indexLabel: "\u2193 lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
-                { y: 500 },
-                { y: 480 },
-                { y: 510 }
-            ]
+            dataPoints: dataPoints
         }]
     });
     chart.render();
